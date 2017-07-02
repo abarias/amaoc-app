9209eac0e01ae51b9adfc96b3dad8061c543ca16
abarias <abarias23@gmail.com>
2017-07-03 00:05:30 +0800

    Created EF DBContext classes

---
+++ Chevron.ITC.AMAOC.Backend/Chevron.ITC.AMAOC.Backend/Models/AMAOCContextInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Chevron.ITC.AMAOC.Backend.Models
+{
+    public class AMAOCContextInitializer : DropCreateDatabaseIfModelChanges<AMAOCContext>
+    {
+		protected override void Seed(AMAOCContext context)
+		{
+			//Seed Data Here
+		}
+    }
+}
