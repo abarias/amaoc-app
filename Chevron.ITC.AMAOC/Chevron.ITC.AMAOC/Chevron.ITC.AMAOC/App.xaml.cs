f06c6e7eb0712be069515f75dcb50ee1e0d5ca15
abarias <abarias23@gmail.com>
2017-07-06 17:20:09 +0800

    auth modifications

--- Chevron.ITC.AMAOC/Chevron.ITC.AMAOC/Chevron.ITC.AMAOC/App.xaml.cs
+++ Chevron.ITC.AMAOC/Chevron.ITC.AMAOC/Chevron.ITC.AMAOC/App.xaml.cs
@@ -1,28 +1,37 @@
 ﻿using System.Collections.Generic;
-
+using System;
 using Chevron.ITC.AMAOC.Helpers;
 using Chevron.ITC.AMAOC.Services;
 using Chevron.ITC.AMAOC.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.Identity.Client;
+using System.Configuration;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Chevron.ITC.AMAOC
 {
     public partial class App : Application
     {
+        public static PublicClientApplication PCA = null;
         //MUST use HTTPS, neglecting to do so will result in runtime errors on iOS
         public static bool AzureNeedsSetup => AzureMobileAppUrl == "https://cvxitcamaocapp.azurewebsites.net";
         public static string AzureMobileAppUrl = "https://cvxitcamaocapp.azurewebsites.net";
         // Azure AD B2C Coordinates
-        public static string Tenant = "chevronitcama.onmicrosoft.com";
-        public static string ClientID = "7493eff3-078c-4f58-a5f2-effaa18acbfb";
-        public static string PolicySignUpSignIn = "B2C_1_AMAOCAppSignUp";
+        public static string Tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        public static string ClientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        public static string PolicySignUpSignIn = ConfigurationManager.AppSettings["ida:SignUpSignInPolicyId"];
         public static string PolicyEditProfile = "B2C_1_AMAOCAppEditProfile";
-        public static string PolicyResetPassword = "B2C_1_AMAOCAppPassReset";
-        
+        public static string PolicyResetPassword = "B2C_1_AMAOCAppPassReset";        
+        public static string DefaultPolicy = PolicySignUpSignIn;
+
+
+
+
         public static string AuthorityBase = $"https://login.microsoftonline.com/tfp/{Tenant}/";
         public static string Authority = $"{AuthorityBase}{PolicySignUpSignIn}";
+
+        public static UIParent UiParent = null;
 
         public static IDictionary<string, string> LoginParameters => null;
 
@@ -31,6 +40,10 @@ namespace Chevron.ITC.AMAOC
             InitializeComponent();
 
             DependencyService.Register<AzureDataStore>();
+
+            PCA = new PublicClientApplication(ClientID, Authority);
+            //PCA.RedirectUri = $"msal{ClientID}://auth";
+            PCA.RedirectUri = $"com.onmicrosoft.chevronitcama.amaocapp://auth";
 
             SetMainPage();
         }
